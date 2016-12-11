aff9dfc581afbc6f2946ebc0daffa128a785d4b1
De117 <De117@users.noreply.github.com>
2016-12-11 03:21:46 +0100

    Rewrite FeatureExtractor.py to be legible and have sane formatting

--- Code/FeatureExtractor.py
+++ Code/FeatureExtractor.py
@@ -1,113 +1,138 @@
 from dependencies import chess
-import random
-import numpy as np
+from chess import WHITE, BLACK, KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN, \
+        A1, A2, A3, A4, A5, A6, A7, A8, B1, B2, B3, B4, B5, B6, B7, B8, \
+        C1, C2, C3, C4, C5, C6, C7, C8, D1, D2, D3, D4, D5, D6, D7, D8, \
+        E1, E2, E3, E4, E5, E6, E7, E8, F1, F2, F3, F4, F5, F6, F7, F8, \
+        G1, G2, G3, G4, G5, G6, G7, G8, H1, H2, H3, H4, H5, H6, H7, H8
 import ChessANNBoardInterface as cabi
 
+
 def is_empty(board, square):
-	return board.piece_type_at(square) == None
-	
+    return board.piece_type_at(square) is None
+
+
 def get_material_configuration(board):
-	wq = len(board.pieces(chess.QUEEN, chess.WHITE))
-	wr = len(board.pieces(chess.ROOK, chess.WHITE))
-	wb = len(board.pieces(chess.BISHOP, chess.WHITE))
-	wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
-	wp = len(board.pieces(chess.PAWN, chess.WHITE))
-	
-	bq = len(board.pieces(chess.QUEEN, chess.BLACK))
-	br = len(board.pieces(chess.ROOK, chess.BLACK))
-	bb = len(board.pieces(chess.BISHOP, chess.BLACK))
-	bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
-	bp = len(board.pieces(chess.PAWN, chess.BLACK))
-	return [wq, wr, wb, wn, wp, bq, br, bb, bn, bp] 
-	
+    wq = len(board.pieces(QUEEN,  WHITE))
+    wr = len(board.pieces(ROOK,   WHITE))
+    wb = len(board.pieces(BISHOP, WHITE))
+    wn = len(board.pieces(KNIGHT, WHITE))
+    wp = len(board.pieces(PAWN,   WHITE))
+
+    bq = len(board.pieces(QUEEN,  BLACK))
+    br = len(board.pieces(ROOK,   BLACK))
+    bb = len(board.pieces(BISHOP, BLACK))
+    bn = len(board.pieces(KNIGHT, BLACK))
+    bp = len(board.pieces(PAWN,   BLACK))
+    return [wq, wr, wb, wn, wp, bq, br, bb, bn, bp]
+
+
 def get_castling_rights(board):
-	k_white = board.has_kingside_castling_rights(chess.WHITE) \
-					and is_empty(board, chess.F1) \
-					and is_empty(board, chess.G1)
-	
-	q_white = board.has_queenside_castling_rights(chess.WHITE) \
-					and is_empty(board, chess.B1) \
-					and is_empty(board, chess.C1) \
-					and is_empty(board, chess.D1)
-					
-	k_black = board.has_kingside_castling_rights(chess.BLACK) \
-					and is_empty(board, chess.F8) \
-					and is_empty(board, chess.G8)
-					
-	q_black = board.has_queenside_castling_rights(chess.BLACK) \
-					and is_empty(board, chess.B8) \
-					and is_empty(board, chess.C8) \
-					and is_empty(board, chess.D8)
-					
-	return [k_white, q_white, k_black, q_black]
-					
+    k_white = board.has_kingside_castling_rights(WHITE) \
+                    and is_empty(board, F1) \
+                    and is_empty(board, G1)
+
+    q_white = board.has_queenside_castling_rights(WHITE) \
+                    and is_empty(board, B1) \
+                    and is_empty(board, C1) \
+                    and is_empty(board, D1)
+
+    k_black = board.has_kingside_castling_rights(BLACK) \
+                    and is_empty(board, F8) \
+                    and is_empty(board, G8)
+
+    q_black = board.has_queenside_castling_rights(BLACK) \
+                    and is_empty(board, B8) \
+                    and is_empty(board, C8) \
+                    and is_empty(board, D8)
+
+    return [k_white, q_white, k_black, q_black]
+
+
 def extract_global_features(board):
-	assert(board.is_valid())
-	vec = []
-	
-	# White's turn
-	vec.append(board.turn)
-	
-	# Material configuration
-	vec.extend(get_material_configuration(board))
-	
-	# Castling rights
-	vec.extend(get_castling_rights(board))
-	
-	return
+    assert(board.is_valid())
+    vec = []
+
+    # White's turn
+    vec.append(board.turn)
+
+    # Material configuration
+    vec.extend(get_material_configuration(board))
+
+    # Castling rights
+    vec.extend(get_castling_rights(board))
+
+    return
+
 
 def create_feature_vector(listOfPieces, slotsNum):
-	vec = [0]*slotsNum
-	for ele in listOfPieces:
-		vec[ele.index] = 1
-	
-	return vec
-	
+    vec = [0]*slotsNum
+    for ele in listOfPieces:
+        vec[ele.index] = 1
+
+    return vec
+
+
 def extract_piece_centric_features(interface):
-	assert(interface.get_board().is_valid())
-	vec = []
-	
-	list = interface.get_piece(chess.QUEEN, chess.WHITE)
-	v = create_feature_vector(list, 1)
-	vec.extend(v)
-	
-	list = interface.get_piece(chess.BISHOP, chess.WHITE)
-	v = create_feature_vector(list, 2)
-	vec.extend(v)
-	
-	print(vec)
-	return
+    assert(interface.get_board().is_valid())
+    vec = []
+
+    list = interface.get_piece(QUEEN, WHITE)
+    v = create_feature_vector(list, 1)
+    vec.extend(v)
+
+    list = interface.get_piece(BISHOP, WHITE)
+    v = create_feature_vector(list, 2)
+    vec.extend(v)
+
+    print(vec)
+    return
+
 
 def extract_attack_def_maps(board):
-  assert(board.is_valid())
-  
-  value_dict = {
-  'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9, 'K': 4,
-  'p': 1, 'n': 3, 'b': 3, 'r': 5, 'q': 9, 'k': 4}
-  attack_list = []
-  defense_list = []
-  for sq in chess.SQUARES:
-    l = []
-    for attacker in list(board.attackers(board.turn, sq)):
-      l += [value_dict[str(board.piece_at(attacker))]]
-    attack_list += [min(l)] if len(l) != 0 else [0]
-    
-    l = []
-    for defender in list(board.attackers(chess.WHITE if board.turn==chess.BLACK else chess.BLACK, sq)):
-      l += [value_dict[str(board.piece_at(defender))]]
-    defense_list += [min(l)] if len(l) != 0 else [0]
-  
-  print(attack_list+defense_list)
-	
-interface = cabi.ChessANNBoardInterface()
+    assert board.is_valid(), "Invalid board!"
 
-while (True):
-	print(interface.get_board())
-	extract_global_features(interface.get_board())
-	extract_piece_centric_features(interface)
-	extract_attack_def_maps(interface.get_board())
-	
-	interface.make_move()
-	input()
+    value_dict = {'P': 1, 'p': 1,  # pawn
+                  'N': 3, 'n': 3,  # knight
+                  'B': 3, 'b': 3,  # bishop
+                  'R': 5, 'r': 5,  # rook
+                  'Q': 9, 'q': 9,  # queen
+                  'K':15, 'k':15}  # king
+
+    white_attackers_list = []
+    black_attackers_list = []
+
+    for sq in chess.SQUARES:
+        # get attacker positions for each square
+        w_attacker_pos = board.attackers(WHITE, sq)
+        b_attacker_pos = board.attackers(BLACK, sq)
+
+        # find out which pieces they are
+        w_attackers = [board.piece_at(pos).symbol() for pos in w_attacker_pos]
+        b_attackers = [board.piece_at(pos).symbol() for pos in b_attacker_pos]
+
+        # find the lowest-valued attacking piece, if any
+        w_val = min((value_dict[piece] for piece in w_attackers), default=0)
+        b_val = min((value_dict[piece] for piece in b_attackers), default=0)
+
+        white_attackers_list += [ w_val ]
+        black_attackers_list += [ b_val ]
+
+    # determine who is doing the attacking
+    attack_list = white_attackers_list if board.turn else black_attackers_list
+    defense_list = black_attackers_list if board.turn else white_attackers_list
+
+    print(attack_list+defense_list)
 
 
+if __name__=="__main__":
+
+    interface = cabi.ChessANNBoardInterface()
+
+    while (True):
+        print(interface.get_board())
+        extract_global_features(interface.get_board())
+        extract_piece_centric_features(interface)
+        extract_attack_def_maps(interface.get_board())
+
+        interface.make_move()
+        input()

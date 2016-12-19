c19d1ac00e7d60935f05e0a4a4faa4290df080d3
De117 <De117@users.noreply.github.com>
2016-12-19 05:18:36 +0100

    Rewrite and reformat the neural network implementation
    The new NN class is now a simple 2-layer network; this will be slightly
    extended later on. Parameter initialization and the optimizer were
    changed, and a (temporary) simple test/demo was added.
    
    It turns out the network works better with one-hot coded outputs.
    All in all, this should be much cleaner.

--- Code/Network.py
+++ Code/Network.py
@@ -1,74 +1,169 @@
-# definitions
-
 import tensorflow as tf
 import numpy as np
 
 
 def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
+    initial = tf.random_uniform(shape, 0.0, 1.0)
+    return tf.Variable(initial)
 
 def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+    initial = tf.random_uniform(shape, 0.095, 0.105)
+    return tf.Variable(initial)
+
+def LeakyReLU(x, alpha=1e-1):
+    return tf.maximum(alpha*x, x)
 
 
-class FCLayer():
-  def __init__(self, inputs, outputs, nonlinearity = tf.nn.relu):
-    self.W = weight_variable([inputs, outputs])
-    self.b = bias_variable([outputs])
-    self.nonlinearity = nonlinearity
-  
-  def forward(self, X):
-    s = tf.matmul(X, self.W) + self.b
-    return self.nonlinearity(s)
+class FCLayer:
+    """A fully connected layer.
 
-import pdb
-class GroupLayer():
-  def __init__(self, input_groups, output_groups, nonlinearity = tf.nn.relu):
-    self.ingroups = [sum(input_groups[:i]) for i in range(len(input_groups) + 1)]
-    self.sublayers = [FCLayer(input_groups[i], output_groups[i], nonlinearity) for i in range(0,len(input_groups))]
-  
-  def forward(self, X):
-    h = [];
-    for i in range(len(self.sublayers)):
-      h += [self.sublayers[i].forward(X[:,self.ingroups[i] : self.ingroups[i+1]])]
-    return tf.concat(1,h)
+    INPUT_DIM  -- input vector dimensionality
+    OUTPUT_DIM -- output vector dimensionality
+    """
+    def __init__(self, input_dim, output_dim, nonlinearity=tf.nn.relu):
+        """
+        Arguments:
+            input_dim  -- input vector dimensionality
+            output_dim -- output vector dimensionality
+            nonlinearity -- the activation function (a tensorflow op)
+        """
+        self.W = weight_variable([output_dim, input_dim])
+        self.b = bias_variable([output_dim])
+        self.nonlinearity = nonlinearity
+        self.INPUT_DIM = input_dim
+        self.OUTPUT_DIM = output_dim
+
+    def forward(self, X):
+        """Forwards the input vector through this fully connected layer.
+        """
+        s = tf.matmul(X, tf.transpose(self.W)) + self.b
+        return self.nonlinearity(s)
+
+
+class GroupLayer:
+    """A custom locally-connected layer.
+    It consists of fully-connected local parts (sublayers).
+    Basically, several FC layers in parallel.
+
+    INPUT_DIM  -- input vector dimensionality
+    OUTPUT_DIM -- output vector dimensionality
+    """
+
+    def __init__(self, input_groups, output_groups, nonlinearity=tf.nn.relu):
+        """
+        Arguments:
+            input_groups  -- a list of sublayer input sizes
+            output_groups -- a list of sublayer output sizes
+            nonlinearity  -- the activation function (a tensorflow op)
+        """
+        # determine boundaries of locally connected regions
+        self.boundaries = [sum(input_groups[:i])
+                            for i in range(len(input_groups)+1)]
+
+        self.INPUT_DIM = self.boundaries[-1]        # informative,
+        self.OUTPUT_DIM = sum(output_groups)        #   really.
+
+        self.sublayers = []
+        for (ingroup, outgroup) in zip(input_groups, output_groups):
+            self.sublayers += [FCLayer(ingroup, outgroup, nonlinearity)]
+
+
+    def forward(self, X):
+        """Forwards the input vector through this locally-connected layer.
+
+        Arguments:
+            X -- a tensorflow tensor/placeholder of appropriate dimensions
+                 (N x INPUT_DIM, where N is the number of individual samples,
+                  i.e. they are laid out in rows)
+        """
+        h = []
+        for i, subl in enumerate(self.sublayers):
+            h += [subl.forward(X[: , self.boundaries[i] : self.boundaries[i+1]])]
+        return tf.concat(1, h)
+
 
 # group_dimens - 2D matrica, u prvom retku su dimenzije ulaza svake grupe
 # a u drugom dimenzije izlaza
 # [[10, 10, 20], [4, 4, 1]] će biti sloj sa 40 ulaza i 9 izlaza
 # prva vrijednost u architecture mora biti jednaka sumi prve dimenzija group_dimens
 # druga vrijednost u architecture je jednaka sumi vrijednosti druge dimenzije group_dimens
-class GroupNetwork():
-  def __init__(self, architecture, group_dimens, nonlinearity = tf.nn.relu):
-    self.layers = [GroupLayer(group_dimens[0], group_dimens[1], nonlinearity=nonlinearity)]
-    
-    for i in range(1, len(architecture)-2):
-      self.layers.append(FCLayer(architecture[i], architecture[i+1], nonlinearity=nonlinearity))
-    
-    self.layers.append(FCLayer(architecture[-2], architecture[-1], nonlinearity=tf.tanh))
-    
-    self.inputs = tf.placeholder(tf.float32, shape=[None, sum(group_dimens[0])])
-    self.output = tf.placeholder(tf.float32, shape= [None, architecture[-1]])
-    
-    self.out = self.layers[0].forward(self.inputs)
-    for i in range(1, len(self.layers)):
-      self.out = self.layers[i].forward(self.out)
-      
-    self.session = tf.Session();
-    self.session.run(tf.initialize_all_variables())
-  
-  def train(self, X, Y_, lr, niter):
-    mse = tf.reduce_sum((self.out - self.output)**2)
-    train_step = tf.train.GradientDescentOptimizer(lr).minimize(mse)
-    
-    errs = [];
-    for i in range(niter):
-      train_step.run(feed_dict={self.inputs: X, self.output: Y_},session=self.session)
-      errs.append(mse.eval(feed_dict={self.inputs: X, self.output: Y_},session=self.session))
-    
-    return errs;
-  
-  def predict(self, X):
-    return self.out.eval(feed_dict={self.inputs: X}, session = self.session)
+class NNetwork:
+    """A 2-layer neural network."""
+
+    def __init__(self, LC_groups, FC_dim, learning_rate):
+
+        (LC_ingroups, LC_outgroups) = LC_groups
+        D = sum(LC_ingroups)    # input layer dimensionality
+        H = sum(LC_outgroups)   # hidden layer dimensionality
+        C = FC_dim              # output dimensionality
+
+        self.layers = [GroupLayer(LC_ingroups, LC_outgroups,
+                                      nonlinearity=tf.nn.relu)]
+        self.layers += [FCLayer(H, C, nonlinearity=tf.sigmoid)]
+
+        # samples
+        self.X = tf.placeholder(tf.float32, [None, D])    # N x D
+        self.Y = tf.placeholder(tf.float32, [None, C])    # N x C
+
+        # output
+        self.out = self.layers[0].forward(self.X)
+        self.out = self.layers[1].forward(self.out)
+
+        # loss
+        self.loss = tf.reduce_sum((self.out - self.Y)**2)
+
+        # train step & session
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+        optimizer = tf.train.AdamOptimizer(learning_rate)
+        self.train_step = optimizer.minimize(self.loss)
+        self.session = tf.Session()
+
+
+    def train(self, X, Y_, n_iter):
+        self.session.run( tf.initialize_all_variables() )
+
+        errs = [];
+        for i in range(n_iter):
+            feed_dict = {self.X: X, self.Y: Y_}
+            fetches = [self.loss, self.train_step]
+
+            (loss, _) = self.session.run(fetches, feed_dict=feed_dict)
+            errs += [loss]
+
+            if i%10==0 or i==n_iter-1:
+                t = self.predict(X)
+                print("%d: %f %f %f %f" % \
+                        (i, t[0][0], t[1][0], t[2][0], t[3][0]))
+                print("  err=%f" % (errs[-1]))
+
+        return errs;
+
+
+    def predict(self, X):
+        return self.out.eval(feed_dict={self.X: X}, session=self.session)
+
+
+def test():
+    X = np.array([
+        [0,0],
+        [0,1],
+        [1,0],
+        [1,1]])
+    Y = np.array([
+        [1,0],
+        [0,1],
+        [0,1],
+        [1,0]])
+    #Y = np.array([[0,1,1,0]]).T
+
+    tf.set_random_seed(42)
+
+    eta=1e-2; niter=1000
+    print("lr={}, niter={}".format(eta, niter))
+
+    net = NNetwork(LC_groups=([2],[2]), FC_dim=2, learning_rate=eta)
+    errs = net.train(X, Y, n_iter=niter)
+    print(net.predict(X))
+
+if __name__=="__main__":
+    test()
